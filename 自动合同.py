62d3dd372774204c388b80663ddd56ffec754fed
QingyanSu <345885398@.qq.com>
2021-01-29 17:00:01 +0800

    v1.0.1
    1、解决路径问题
    2、确认float_to_str函数except的else不能简化：使excel中值为100时，word输出不为100.00，可能因excel中默认数值有两位小数，即100.00显示为100

--- 自动合同.py
+++ 自动合同.py
@@ -1,22 +1,22 @@
 import xlrd  # 引入Excel读取模块
 from mailmerge import MailMerge  # 引用邮件处理模块
 
-def float_to_str(float_value):
+def float_to_str(float_value):   #定义浮点转字符函数
     try:
-        float_value = int(float_value)
-    except ValueError as e:
-        return float_value
+        float_value = int(float_value)    #尝试转换为整数
+    except ValueError as e:    #文本或文本含数字情况赋值失败
+        return float_value     #即无需转换，直接返回
     else:
-        if (float_value == int(float_value)):
-            return str(int(float_value))
-        else :
-            return str(float_value)
+        if (float_value == int(float_value)):  #如果值为整数
+            return str(int(float_value))       #值转换为整数后转换为字符
+        else :                                 #如果值为浮点数
+            return str(float_value)            #值转换为字符
       
-datafile_path = 'D:/庞克时代测试版/合同Test.xlsx'  # 表格位置
+datafile_path = '合同Test.xlsx'  # 表格位置
 data = xlrd.open_workbook(datafile_path)  # 获取数据
 table = data.sheet_by_name('合同数据（勿动）')  # 表格内工作表
 ncols = table.ncols
-template = 'D:/庞克时代测试版/基础设施项目分包合同（范本）2020自动版.docx'  # 模版位置
+template = '基础设施项目分包合同（范本）2020自动版.docx'  # 模版位置
 document = MailMerge(template)
 
 for i in range(ncols):  # 循环逐行打印
